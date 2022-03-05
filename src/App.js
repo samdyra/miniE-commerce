b141289ba43c1cde445c349c82ab36d5376b9de4
samdyra <197413vr@gmail.com>
2022-03-05 16:28:30 +0700

    install fonts and third party stying

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import HeaderScreen from "./Header/Header";
 import NavBar from "./Navbar/Navbar";
 
 function App() {
   return (
     <div>
       <NavBar></NavBar>
+      <HeaderScreen></HeaderScreen>
     </div>
   );
 }
---
+++ src/Header/Header.js
@@ -0,0 +1,16 @@
+const HeaderScreen = () => {
+  return (
+    <div className="header-container">
+      <div className="wrapper">
+        <header>
+          <div className="hero-content">
+            <h1>Enhance Your Smile</h1>
+            <a>CLICK HERE</a>
+          </div>
+        </header>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderScreen;
